4d0cd1bb5d7516085f123778edc6d3b3afd83129
pietrocruciata <cruciata9748@gmail.com>
2024-03-07 16:48:36 +0100

    add: interactive html

--- dadi/js/app.js
+++ dadi/js/app.js
@@ -1,14 +1,17 @@
 //GENERARE UN NUMERO CASUALE DA 1 A 6 PER L'UTENTE
-const userRoll = document.getElementById('dadouser')
+const userRoll = document.querySelector('.dadouser')
 userRoll.addEventListener('click' , function(){
     const numberUser = []
     const numberComputer = []
         const randomNumbers = Math.floor(Math.random() * 6)+1;
         numberUser.push(randomNumbers)
+        document.getElementById('usernumber').innerHTML = randomNumbers
 
         //GENERARE UN NUMERO CASUALE DA 1 A 6 PER IL COMPUTER
         const randomNumbersComputer = Math.floor(Math.random() * 6)+1;
         numberComputer.push(randomNumbersComputer)
+        document.getElementById('computernumber').innerHTML = randomNumbersComputer
+
     
     console.log('numero user: '+numberUser ,'numero computer: '+ numberComputer );
 
@@ -16,10 +19,16 @@ userRoll.addEventListener('click' , function(){
 
 if(numberUser > numberComputer){
     console.log('HAI VINTO!');
+    document.querySelector('.result').innerHTML = 'HAI VINTO!'
+
 }else if (numberUser < numberComputer){
     console.log('HAI PERSO!');
+    document.querySelector('.result').innerHTML = 'HAI PERSO!'
+
 }else{
-    console.log('SCONTRO TRA TITANI , RILANCIA!!');
+    console.log('SCONTRO TRA TITANI! RILANCIA!!');
+    document.querySelector('.result').innerHTML = ' SCONTRO TRA TITANI! RILANCIA !'
+
 }
 
 
